ebbeba563444e143857c11614138fb5b60d5dc01
evan li <13145046715@163.com>
2021-09-15 18:43:11 +0800

    first commit

---
+++ Demo01.py
@@ -0,0 +1,26 @@
+# -*- coding = utf-8  -*-
+#@Time:2021/9/8 17:48
+#@Author:Li LeiLei
+#@File:Demo01.py
+
+result = 20
+
+result += 10
+
+# print(result)
+
+age = 18
+
+# assert  18 < age < 50, "age 变量内容处理错误"
+#
+# print("您的年龄是：%d" %age)
+
+sum  = 0
+num  = 1
+
+while num < 100:
+    sum += num
+    num += 1
+
+
+print(sum)
